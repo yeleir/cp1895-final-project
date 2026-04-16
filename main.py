fa26eebb475488a10e61d4b059efcfc73772323c
yeleir <rieleypelley@gmail.com>
2026-04-16 02:23:34 -0230

    Finished Add Game logic, image uploads and built Display page.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, flash
 from flask_session import Session
+import os
 
 app = Flask(__name__)
 
@@ -9,16 +10,39 @@ app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = "filesystem"
 Session(app)
 
+save_location = "static/images"
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/games')
 def display_games():
-    return render_template('games.html')
+    vault_data = session.get("games", {})
+    return render_template('games.html', vault=vault_data)
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_game():
+    if request.method == 'POST':
+        title = request.form['title']
+        genre = request.form['genre']
+        rating = request.form['rating']
+        review = request.form['review']
+        image_file = request.files['image']
+
+        if image_file.filename != '':
+            save_file_name = os.path.join(save_location, image_file.filename)
+            image_file.save(save_file_name)
+
+        if "games" in session:
+            session["games"][title] = {'genre': genre, 'rating': rating, 'review': review, 'image': image_file.filename}
+        else:
+            session["games"] = {}
+            session["games"][title] = {'genre': genre, 'rating': rating, 'review': review, 'image': image_file.filename}
+
+        flash(f"'{title}' added to the vault!")
+        return render_template('add.html')
+
     return render_template('add.html')
 
 @app.route('/remove')
